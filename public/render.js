59108f4a5c3d37386e087c478e344e5dd03625f1
Trent Houliston <trent@houliston.me>
2016-04-30 00:11:27 +1000

    Fixes

--- public/render.js
+++ public/render.js
@@ -1,15 +1,19 @@
 var canvas = document.getElementById('canvas');
 var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
-var camera = new THREE.OrthographicCamera(0, 1, 1, 0, 1, 1000);
+// var camera = new THREE.OrthographicCamera(0, 1, 1, 0, 1, 1000);
+var camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientWidth, 0.0001, 10000);
+
 
 var geometry = new THREE.PlaneGeometry(1, 1);
-geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0.5, 0.5, 0));
+// geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0.5, 0.5, 0));
 var material = new THREE.ShaderMaterial({
   uniforms: {
     width: { type: 'i', value: 1280 },
     height: { type: 'i', value: 1024 },
-    image: { type: 't', value: new THREE.TextureLoader().load( "data/LightHue.png") }
+    image: { type: 't', value: new THREE.TextureLoader().load("data/LightHue.png", function() {
+      render();
+    }) }
   },
   vertexShader: document.getElementById('vertex-shader').textContent,
   fragmentShader: document.getElementById('fragment-shader').textContent
@@ -24,7 +28,7 @@ camera.position.z = 1;
 render();
 
 function render() {
-  requestAnimationFrame(render);
+  // requestAnimationFrame(render);
 
   if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
     renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
